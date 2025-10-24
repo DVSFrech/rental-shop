d15262bbe1ee51890976eef9bd6f525056136372
DVSFrech <info@dvsfrech.de>
2025-10-24 15:33:45 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,38 @@
 export default function Home() {
   return (
-    <div style={{
-      textAlign: 'center',
-      fontFamily: 'Arial, sans-serif',
-      marginTop: '100px',
-      lineHeight: '1.8'
-    }}>
-      <h1>Willkommen bei DVSFrech </h1>
+    <div
+      style={{
+        textAlign: "center",
+        fontFamily: "Arial, sans-serif",
+        marginTop: "100px",
+        lineHeight: "1.8",
+      }}
+    >
+      <h1>Willkommen bei DVSFrech</h1>
       <p>Unsere Internetseite befindet sich derzeit im Aufbau.</p>
-      <p>Für Mietanfragen können Sie uns gerne telefonisch oder per E-Mail kontaktieren:</p>
-      <ul style={{ listStyle: 'none', padding: 0 }}>
-        <li>📞 <strong>Telefon:</strong> <a href="tel:01712956950">0171&nbsp;2956950</a></li>
-        <li>📧 <strong>E-Mail:</strong> <a href="mailto:info@dvsfrech.de">info@dvsfrech.de</a></li>
+      <p>
+        Für Mietanfragen können Sie uns gerne telefonisch oder per E-Mail
+        kontaktieren:
+      </p>
+      <ul style={{ listStyle: "none", padding: 0 }}>
+        <li>
+          📞 <strong>Telefon:</strong>{" "}
+          <a href="tel:01712956950">0171 2956950</a>
+        </li>
+        <li>
+          📧 <strong>E-Mail:</strong>{" "}
+          <a href="mailto:info@dvsfrech.de">info@dvsfrech.de</a>
+        </li>
       </ul>
       <p>
-        Alternativ können Sie auch weiterhin unseren Webshop über{' '}
+        Alternativ können Sie auch weiterhin unseren Webshop über{" "}
         <a
           href="https://www.kleinanzeigen.de/pro/dvsfrech"
           target="_blank"
           rel="noopener noreferrer"
         >
           kleinanzeigen.de
-        </a>{' '}
+        </a>{" "}
         nutzen.
       </p>
       <p>Ihr DVSFrech-Team</p>
