216ccbea5ad8fb30abda205476c7a6287707843d
DVSFrech <info@dvsfrech.de>
2025-10-24 14:12:17 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px", fontFamily: "sans-serif" }}>
+      <h1>Willkommen bei DVSFrech Vermietshop 🎉</h1>
+      <p>Deine neue Onlineplattform ist erfolgreich verbunden!</p>
+      <p>Hier kannst du bald dein Equipment vermieten.</p>
+    </div>
+  );
+}
